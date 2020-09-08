ae8c40d05a4c243e3ecac63cba8234926768a5bd
alexandr_sadunenko <a.sadunenko@gmail.com>
2020-09-08 16:19:48 +0300

    Pagination

--- CocktailDB/View/FiltersViewController.swift
+++ CocktailDB/View/FiltersViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class FiltersViewController: UIViewController {
 
+    var categories: CategoryModel!
+    var chekEnable: [Bool]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func applyTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "unwindSegueToDrinks", sender: sender)
+    }
+}
+
+extension FiltersViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+ 
+}
+
+extension FiltersViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.drinks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = categories.drinks[indexPath.row].strCategory
+        //cell.accessoryType = .checkmark
+        cell.accessoryType = chekEnable[indexPath.row] ? .checkmark : .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = !chekEnable[indexPath.row] ? .checkmark : .none
+            chekEnable[indexPath.row] = !chekEnable[indexPath.row]
+        }
+    }
+    
 }
--- CocktailDB/View/ViewController.swift
+++ CocktailDB/View/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - Properties
+    
     var categories: CategoryModel!
     var drinks: DrinksModel!
     var drinkImages = [UIImage]()
+    var chekEnable: [Bool]!
+    var sectionData = [String: DrinksModel]()
     var networking = NetworkServiceManager()
+
+    //MARK: - IBOutlets
 
     @IBOutlet weak var tableView: UITableView!
     
+    //MARK: - IBActions
+    
     @IBAction func filterTapped(_ sender: UIBarButtonItem) {
-        tableView.reloadData()
-        print(categories.drinks)
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        guard let source = segue.source as? FiltersViewController else { return }
+        chekEnable = source.chekEnable
     }
     
     override func viewDidLoad() {
@@ -29,6 +40,7 @@ class ViewController: UIViewController {
             
     }
 }
+
 // MARK: - Networking
 
 extension ViewController {
@@ -41,6 +53,7 @@ extension ViewController {
         queue.async(group: group) {
             self.networking.dataFetcherCategory(andpoint: "list.php?c=list") { (currentModel) in
                 self.categories = currentModel
+                self.chekEnable = Array(repeating: true, count: self.categories.drinks.count)
                 group.leave()
             }
         }
@@ -63,7 +76,12 @@ extension ViewController {
     
     func getPartDrinks(category: String) {
         networking.dataFetcherDrinks(andpoint: "filter.php?c=\(category)") { (currentModel) in
-            self.drinks = currentModel
+            if self.drinks == nil {
+                self.drinks = currentModel
+            } else {
+                self.drinks.drinks += currentModel.drinks
+            }
+            self.sectionData[category] = currentModel
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
@@ -72,14 +90,38 @@ extension ViewController {
     
 }
 
+//MARK: - Table View Data Source
+
 extension ViewController: UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        guard let categories = self.categories else { return 0 }
+        return categories.drinks.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        guard let categories = self.categories else { return "" }
+        return categories.drinks[section].strCategory
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let drinks = self.drinks else { return 0 }
-        return drinks.drinks.count
+        
+        //guard let drinks = self.drinks else { return 0 }
+        //return drinks.drinks.count
+        
+        let key = categories.drinks[section].strCategory
+        if let value = sectionData[key] {
+            return value.drinks.count
+        }
+        
+        return 0
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let drinks = self.drinks else { return UITableViewCell() }
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! DrinkTableViewCell
         cell.nameLabel.text = drinks.drinks[indexPath.row].strDrink
@@ -88,9 +130,57 @@ extension ViewController: UITableViewDataSource {
                 cell.drinkImage.image = image
             }
         })
-        //cell.drinkImage.image
-            //drinkImages = networking.dataFetcherImage(andpoint: <#T##String#>, complition: <#T##(UIImage) -> Void#>)
+        
         return cell
+    }
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        print(section)
+        let key = categories.drinks[section].strCategory
+        if let value = sectionData[key] {
+            //print(value)
+        } else {
+            //getPartDrinks(category: key)
+        }
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let key = categories.drinks[indexPath.section].strCategory
+        if let value = sectionData[key] {
+            if indexPath.row == value.drinks.count - 1 {
+                print(indexPath.row)
+                print(indexPath.section)
+                if categories.drinks.count > indexPath.section + 1 {
+                    let key = categories.drinks[indexPath.section + 1].strCategory
+                    if let value = sectionData[key] {
+                        //print(value)
+                    } else {
+                        getPartDrinks(category: key)
+                    }
+                }
+            }
+        }
     }
     
 }
+
+//MARK: - Table View Delegate
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+}
+
+//MARK: - Segue
+
+extension ViewController {
+ 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueToFilters" {
+            guard let destination = segue.destination as? FiltersViewController else { return }
+            destination.categories = self.categories
+            destination.chekEnable = chekEnable
+        }
+    }
+}
+
